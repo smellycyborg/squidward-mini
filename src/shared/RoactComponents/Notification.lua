810fe93d4a3537c35ae179255e735f8f5e132870
chavez <chavez.swagger@gmail.com>
2023-08-04 13:05:48 -0500

    edit notification design

--- src/shared/RoactComponents/Notification.lua
+++ src/shared/RoactComponents/Notification.lua
@@ -9,10 +9,13 @@ return function(props)
 
     return Roact.createElement("TextLabel", {
         AnchorPoint = Vector2.new(0.5, 0.5),
-        BackgroundTransparency = 1,
-        Position = UDim2.fromScale(0.5, 0.5),
+        BackgroundTransparency = 0.6,
+        BackgroundColor3 = Color3.fromRGB(255, 255, 255),
+        Position = UDim2.fromScale(0.75, 0.5),
         Size = UDim2.fromScale(0.2, 0.1),
         Text = notificationMessage,
+        TextScaled = true,
         TextColor3 = Color3.fromRGB(156, 41, 232),
+        TextStrokeTransparency = 0.6,
     })
 end
