6f0a19babf44d8561e076c1857825710db860783
chavez <chavez.swagger@gmail.com>
2023-08-04 08:41:50 -0500

    add main container

---
+++ src/shared/RoactComponents/MainContainer.lua
@@ -0,0 +1,37 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Packages = ReplicatedStorage.Packages
+local Common = ReplicatedStorage.Common
+local RoactComponents = Common.RoactComponents
+
+local Roact = require(Packages.ReplicatedStorage)
+local Countdown = require(RoactComponents.Countdown)
+local Notification = require(RoactComponents.Notification)
+
+local MainContainer = Roact.Component:extend("MainContainer")
+
+function MainContainer:render()
+    local countdownTime = self.props.countdownTime
+    local notificationMessage = self.props.notificationMessage
+
+    return Roact.createElement("ScreenGui", {
+        ResetOnSpawn = false,
+    }, {
+       Holder = Roact.createElement("Frame", {
+        AnchorPoint = Vector2.new(0.5, 0.5),
+        BackgroundTransparency = 1,
+        Position = UDim2.fromScale(0.5, 0.5),
+        Size = UDim2.fromScale(0.8, 1),
+        SizeConstraint = Enum.SizeConstraint.RelativeXX,
+       }, {
+            Countdown = Roact.createElement(Countdown, {
+                countdownTime = countdownTime,
+            }),
+            Notification = Roact.createElement(Notification, {
+                notificationMessage = notificationMessage,
+            })
+       })
+    })
+end
+
+return MainContainer
