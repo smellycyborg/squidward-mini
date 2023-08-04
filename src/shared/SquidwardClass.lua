936f605e2e9d2b3a33790be86106e0f99aa0394a
chavez <chavez.swagger@gmail.com>
2023-08-04 10:49:30 -0500

    add pathfinding

--- src/shared/SquidwardClass.lua
+++ src/shared/SquidwardClass.lua
@@ -1,4 +1,5 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
 
 local Packages = ReplicatedStorage.Packages
 local Characters = ReplicatedStorage.Characters
@@ -11,11 +12,55 @@ local squidward = {}
 local squidwardPrototype = {}
 local squidwardPrivate = {}
 
+local function _getClosestCharacter(modelPosition)
+    local distances = {}
+
+	for _, player in ipairs(Players:GetPlayers()) do
+		local character = player.Character
+		if not character then
+			continue
+		end
+		
+		local humanoid = character:FindFirstChild("Humanoid")
+		if not humanoid then
+			continue
+		end
+		
+		if humanoid.Health <= 0 then
+			continue
+		end
+
+        local humanoidRootPart = character:FindFirstChild("HumanoidRootPart")
+        if not humanoidRootPart then
+            continue
+        end
+
+        local characterPosition = humanoidRootPart.Position
+
+        local distanceBetweenZombieAndPlayer = (zombiePosition - characterPosition).Magnitude
+        distances[character] = distanceBetweenZombieAndPlayer
+	end
+
+    local sortedKeys = {}
+	for key in pairs(distances) do
+		table.insert(sortedKeys, key)
+	end
+
+	table.sort(sortedKeys, function(a, b)
+		return distances[a] < distances[b]
+	end)
+
+    local closestCharacter = sortedKeys[1]
+
+    return closestCharacter
+end
+
 function squidward.new()
     local self = {}
     local private = {}
 
     self.healthChanged = Signal.new()
+    self.hasDied = Signal.new()
 
     private.health = 100
     private.workspaceModel = nil
@@ -34,6 +79,20 @@ function squidwardPrototype:spawn()
     private.workspaceModel = squidwardClone
 end
 
+function squidwardPrototype:findPath()
+    local private = squidwardPrivate[self]
+
+    local workspaceModel = private.workspaceModel
+    local modelPosition = workspaceModel.Position
+    local closestCharacter = _getClosestCharacter(modelPosition)
+    local closestCharacterRootPart = closestCharacter:FindFirstChild("HumanoidRootPart")
+    if not closestCharacterRootPart then
+        return warn("Attempt to index nil with root part.")
+    end
+
+    workspaceModel:MoveTo(closestCharacterRootPart.Position)
+end
+
 function squidwardPrototype:addHealth(amount)
     local private = squidwardPrivate[self]
 
@@ -46,6 +105,12 @@ function squidwardPrototype:subtractHealth(amount)
 
     private.health -= amount
     self.healthChanged:Fire(private.health)
+end
+
+function squidwardPrototype:getWorkspaceModel()
+    local private = squidwardPrivate[self]
+
+    return private.workspaceModel
 end
 
 function squidwardPrototype:destroy()
