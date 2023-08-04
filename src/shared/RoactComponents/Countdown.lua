161d4858f0a85898a67e27b2677d4a46f823fead
chavez <chavez.swagger@gmail.com>
2023-08-04 08:41:40 -0500

    add countdown component

---
+++ src/shared/RoactComponents/Countdown.lua
@@ -0,0 +1,20 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Packages = ReplicatedStorage.Packages
+
+local Roact = require(Packages.ReplicatedStorage)
+
+local COUNTDOWN_MESSAGE = "Countdown:  "
+
+return function(props)
+    local countdownTime = props.countdownTime
+
+    local text = COUNTDOWN_MESSAGE .. countdownTime
+
+    return Roact.createElement("TextLabel", {
+        AnchorPoint = Vector2.new(0.5, 0),
+        Position = UDim2.fromScale(0.5, 0),
+        Size = UDim2.fromScale(0.2, 0.1),
+        Text = text,
+    })
+end
