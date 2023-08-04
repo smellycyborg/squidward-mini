1fe9bd70567ce3fd7c163994d483c23edde57019
chavez <chavez.swagger@gmail.com>
2023-08-04 08:55:54 -0500

    edit ui design

--- src/shared/RoactComponents/Countdown.lua
+++ src/shared/RoactComponents/Countdown.lua
@@ -13,8 +13,14 @@ return function(props)
 
     return Roact.createElement("TextLabel", {
         AnchorPoint = Vector2.new(0.5, 0),
+        BackgroundColor3 = Color3.fromRGB(35, 71, 215),
         Position = UDim2.fromScale(0.5, 0),
         Size = UDim2.fromScale(0.2, 0.1),
         Text = text,
+        TextScaled = true,
+    }, {
+        UICorner = Roact.createElement("UICorner", {
+            CornerRaidus = UDim.new(0.1, 0),
+        })
     })
 end
--- src/shared/RoactComponents/Notification.lua
+++ src/shared/RoactComponents/Notification.lua
@@ -8,9 +8,11 @@ return function(props)
     local notificationMessage = props.notificationMessage
 
     return Roact.createElement("TextLabel", {
-        AnchorPoint = Vector2.new(0.5, 0),
-        Position = UDim2.fromScale(0.5, 0),
+        AnchorPoint = Vector2.new(0.5, 0.5),
+        BackgroundTransparency = 1,
+        Position = UDim2.fromScale(0.5, 0.5),
         Size = UDim2.fromScale(0.2, 0.1),
         Text = notificationMessage,
+        TextColor3 = Color3.fromRGB(156, 41, 232),
     })
 end
